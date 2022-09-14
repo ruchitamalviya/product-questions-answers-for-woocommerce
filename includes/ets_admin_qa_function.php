232857f12b46f9fde6734123e9d8c663d376a4a5
ruchitamalviya <ruchita.malviya.ets@gmail.com>
2022-09-14 19:04:04 +0530

    remove var space

--- includes/ets_admin_qa_function.php
+++ includes/ets_admin_qa_function.php
@@ -550,7 +550,7 @@ class ETS_WOO_PRODUCT_ADMIN_QUESTION_ANSWER
 	 * Secipt File include.
 	 */
 	public function admin_woo_qa_script() {
-		global $pagenow , $post; 
+		global $pagenow, $post; 
 		if ( $pagenow == 'post.php' && $post ) {
 			 
 			$addNewQaNonce = wp_create_nonce('ets-product-add-new-qa');
